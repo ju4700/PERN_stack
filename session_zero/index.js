bb692265422861044921d29633dcc1a2e6749a9a
ju4700 <jalal.dev.design@gmail.com>
2025-12-09 22:53:42 +0600

    Refactor package.json and package-lock.json to move nodemon to devDependencies

--- session_zero/index.js
+++ session_zero/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const {Pool} = require('pg');
+// const {Pool} = require('pg');
 
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-    user: 'ju4700',
-    host: 'localhost',
-    database: 'perndatabse',
-    password: '4700',
-    port: 5432,
-});
+// const pool = new Pool({
+//     user: 'ju4700',
+//     host: 'localhost',
+//     database: 'perndatabse',
+//     password: '4700',
+//     port: 5432,
+// });
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.post('/categories/:id', (req, res) => {
+    res.send('Category created' + req.params.id);
 });
 
 app.listen(port, () => {
