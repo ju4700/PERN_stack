e9e84bd6d73faf7a3ae6ea49cc07486d9593a326
ju4700 <jalal.dev.design@gmail.com>
2025-12-09 22:41:45 +0600

    Add PostgreSQL connection setup and update dependencies

--- session_zero/index.js
+++ session_zero/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const {Pool} = require('pg');
+
 const app = express();
 const port = 3000;
+
+const pool = new Pool({
+    user: 'ju4700',
+    host: 'localhost',
+    database: 'perndatabse',
+    password: '4700',
+    port: 5432,
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
