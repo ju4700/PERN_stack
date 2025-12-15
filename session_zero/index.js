54e31e6e69f1fcd4252f2059c510d6151f8b0725
ju4700 <jalal.dev.design@gmail.com>
2025-12-15 19:36:48 +0600

    refactor: remove unused imports and commented-out user retrieval endpoint

--- session_zero/index.js
+++ session_zero/index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import { z } from 'zod';
-import { prisma } from './database/prisma.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
@@ -11,17 +9,6 @@ app.use(express.static("frontend"));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
-
-
-// app.get('/users', async (req, res) => {
-//     try {
-//         const result = await pool.query('SELECT * FROM "users"');
-//         res.json(result.rows);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send('Error fetching users');
-//     }
-// });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
